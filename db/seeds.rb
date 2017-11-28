bb2fab97f08db0f8d7745e1242477ac0e74f037e
Mssmall <liz_soccer123@hotmail.com>
2017-11-28 15:32:33 +1100

    2nd heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Club.destroy_all
-c1 = Club.create :name => 'Chelsea Football Club', :manager => 'Antonio Conte', :homeground => 'Stamford Bridge'
-c2 = Club.create :name => 'Manchester United', :manager => 'Jose Mourinho', :homeground => 'Old Trafford'
+c1 = Club.create(:name => 'Chelsea Football Club', :manager => 'Antonio Conte', :homeground => 'Stamford Bridge', :image => 'https://platform-static-files.s3.amazonaws.com/premierleague/badges/t8.svg')
+c2 = Club.create(:name => 'Manchester United', :manager => 'Jose Mourinho', :homeground => 'Old Trafford', :image => 'https://platform-static-files.s3.amazonaws.com/premierleague/badges/t1.svg')
 
 User.destroy_all
-u1 = User.create :username => 'Blueforever', :email => 'cfc@live.ft', :password => 'chicken', :password_confirmation => 'chicken'
-u2 = User.create :username => 'ManUrules', :email => 'mun@live.ft', :password => 'chicken', :password_confirmation => 'chicken'
+u1 = User.create(:username => 'Blueforever', :email => 'cfc@live.ft', :password => 'chicken', :password_confirmation => 'chicken', :photo => 'https://www.featurepics.com/StockImage/20140523/soccer-fan-stock-illustration-3073764.jpg')
+u2 = User.create(:username => 'ManUrules', :email => 'mun@live.ft', :password => 'chicken', :password_confirmation => 'chicken', :photo => 'https://www.featurepics.com/StockImage/20140523/soccer-fan-stock-illustration-3073756.jpg')
 
 Post.destroy_all
 p1 = Post.create :message => 'What did you think of the game?'
